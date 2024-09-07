3045d9a6d47502c7d6b2ef23b3e0fd7e9b77c293
Shedi <d.sheludko1809@gmail.com>
2024-09-07 21:39:53 +0300

    Make some Useful Queries SQL

--- AnimalClasses/obj/Debug/net8.0/AnimalClasses.AssemblyInfo.cs
+++ AnimalClasses/obj/Debug/net8.0/AnimalClasses.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimalClasses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5146a807e8b06cd4e86dd8ddb5b6bc6c3de2e0ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3aff983a6e235ff1b0547f7a16b84d7f3e5746")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimalClasses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimalClasses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
